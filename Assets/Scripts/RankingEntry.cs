25fab07e5f232fbe73625495a72c33e42af470cf
Hanibuchi <hanibuchikouto003@gmail.com>
2025-10-12 07:51:57 +0900

    ランキング画面のホバー表示実装

--- Assets/Scripts/RankingEntry.cs
+++ Assets/Scripts/RankingEntry.cs
@@ -1,5 +1,7 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 // RankingManagerで定義した HighScore モデルが必要です。
@@ -8,13 +10,13 @@ using UnityEngine.UI;
 /// <summary>
 /// ランキングリストの1行分の情報を表示するUIコンポーネント。
 /// </summary>
-public class RankingEntry : MonoBehaviour
+public class RankingEntry : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
     [Header("UI References")]
     [SerializeField] private TextMeshProUGUI rankText;
     [SerializeField] private TextMeshProUGUI scoreText;
     [SerializeField] private TextMeshProUGUI usernameText;
-
+    string _hoverMessage;
 
     /// <summary>
     /// ランキングエントリに情報を設定し、UIに表示します。
@@ -27,11 +29,59 @@ public class RankingEntry : MonoBehaviour
         // スコアはlong型なので、適切な形式で表示します。
         scoreText.text = data.Score.ToString("N0"); // 例: 1,234,567 の形式
         usernameText.text = data.Username;
+        _hoverMessage = $"{GameManager.Instance.CalcRarity(data.Score).ToString("N0")}回に1回\n{data.CreatedAt.ToString("yyyy/MM/dd H時m分s秒")}";
         if (isMyScore)
         {
             rankText.fontStyle = FontStyles.Underline;
             scoreText.fontStyle = FontStyles.Underline;
             usernameText.fontStyle = FontStyles.Underline;
+        }
+    }
+
+
+    public void OnDrag(PointerEventData eventData)
+    {
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        OpenHoverUI();
+    }
+
+    // === IPointerEnterHandler (ホバー開始時) ===
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        // (1) ホバーメッセージを生成・表示
+        if (Pointer.current.press.isPressed)
+        {
+            OpenHoverUI();
+        }
+    }
+
+    // === IPointerExitHandler (ホバー終了時) ===
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        OpenHoverUI(false);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        OpenHoverUI(false);
+    }
+
+    HoverUI hoverUI;
+
+    void OpenHoverUI(bool open = true)
+    {
+        if (hoverUI != null)
+        {
+            hoverUI.DestroySelf();
+            hoverUI = null;
+        }
+        if (open)
+        {
+            hoverUI = Instantiate(GameManager.Instance.HoverUIPrefab).GetComponent<HoverUI>();
+            hoverUI.SetMessage(_hoverMessage);
         }
     }
 }
